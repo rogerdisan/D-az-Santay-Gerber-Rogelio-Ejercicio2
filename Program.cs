7423618ecce4fb26c77fdf08da18f76d9bd69c63
Gerber Rogelio Díaz Santay <rogerdisan18@gmail.com>
2026-02-22 21:39:22 -0600

    Menú y Entradas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("=== Control de Acceso Laboratorio ===");
+Console.WriteLine("1. Estudiante");
+Console.WriteLine("2. Docente");
+Console.WriteLine("3. Tecnico IT");
+Console.WriteLine("4. Visitante");
+Console.Write("Seleccione rol: ");
+
+int rol = int.Parse(Console.ReadLine());
